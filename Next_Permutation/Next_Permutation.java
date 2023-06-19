93d627cb76f501cd11d9b14679f6235f0998506c
QuenWi <116632832+QuenWi@users.noreply.github.com>
2023-06-19 21:34:42 +0200

    next permutation

--- Next_Permutation/Next_Permutation.java
+++ Next_Permutation/Next_Permutation.java
@@ -1,7 +1,84 @@
 package Next_Permutation;
 
+import Helpers.PrintArrayOrList;
+
+import java.io.PrintStream;
+
 public class Next_Permutation {
 
     //Gets the next Permutation
-    
+    public static <T extends Number> void next_Permutation(T[] array){
+        int number = -1;
+        T memory;
+        for(int i = array.length-2; i >= 0; i--){
+            if(compare(array[i], array[i+1])){
+                number = i;
+                break;
+            }
+        }
+        if(number == -1){
+            for (int i = 0; i < array.length/2; i++){
+                memory = array[i];
+                array[i] = array[array.length-1-i];
+                array[array.length-1] = memory;
+            }
+        } else{
+            for (int i = 0; i < array.length; i++){
+                if(compare(array[number], array[i])){
+                    memory = array[number];
+                    array[number] = array[i];
+                    array[i] = memory;
+                    break;
+                }
+            }
+            for (int i = number+1; i < (array.length+number+1)/2; i++){
+                memory = array[i];
+                array[i] = array[array.length-i+number];
+                array[array.length-i+number] = memory;
+            }
+        }
+    }
+
+
+    public static <T extends Number> boolean compare(T nr1, T nr2){
+        boolean result = false;
+        if(nr1 instanceof Float){
+            if(nr1.floatValue() < nr2.floatValue()){
+                return true;
+            } else{
+                return false;
+            }
+        } else if(nr1 instanceof Double){
+            if(nr1.doubleValue() < nr2.doubleValue()){
+                return true;
+            } else{
+                return false;
+            }
+        } else if(nr1 instanceof Integer){
+            if(nr1.intValue() < nr2.intValue()){
+                return true;
+            } else{
+                return false;
+            }
+        } else if(nr1 instanceof Short){
+            if(nr1.longValue() < nr2.longValue()){
+                return true;
+            } else{
+                return false;
+            }
+        } else if(nr1 instanceof Byte){
+            if(nr1.byteValue() < nr2.byteValue()){
+                return true;
+            } else{
+                return false;
+            }
+        } else if(nr1 instanceof Short){
+            if(nr1.shortValue() < nr2.shortValue()){
+                return true;
+            } else{
+                return false;
+            }
+        }
+        return result;
+    }
 }
--- Next_Permutation/Test_Next_Permutation.java
+++ Next_Permutation/Test_Next_Permutation.java
@@ -1,5 +1,19 @@
 package Next_Permutation;
 
+import Helpers.PrintArrayOrList;
+
 public class Test_Next_Permutation {
 
+    public static void main(String[] args) {
+        Float[] array = {1.3f, 3f, 6f, 3.4f, 5.5f, 8.3f, 6.3f, 2f, 1f};
+
+        System.out.println("Given: " + PrintArrayOrList.arrayToString(array));
+        Next_Permutation.next_Permutation(array);
+        System.out.println("Result: " + PrintArrayOrList.arrayToString(array));
+
+        Integer[] array2 = {3, 2, 1};
+        System.out.println("\nSecond\nGiven: " + PrintArrayOrList.arrayToString(array2));
+        Next_Permutation.next_Permutation(array2);
+        System.out.println("Result: " + PrintArrayOrList.arrayToString(array2));
+    }
 }
