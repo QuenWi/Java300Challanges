14e3ee8932d54893a5179c595380c5961ef69773
QuenWi <116632832+QuenWi@users.noreply.github.com>
2023-06-23 15:32:27 +0200

    finish Guess Number higher or lower

--- Guess_Number_Higher_or_Lower/Test_Guess_Number_Higher_or_Lower.java
+++ Guess_Number_Higher_or_Lower/Test_Guess_Number_Higher_or_Lower.java
@@ -8,6 +8,22 @@ public class Test_Guess_Number_Higher_or_Lower {
         Integer max = 1000;
         Guess_Number_Higher_or_Lower game = new Guess_Number_Higher_or_Lower(max);
 
-        //binary Search
+        int guess = max/2;
+        int min = 0;
+        while(game.Guess(guess) != 0){
+            if(game.Guess(guess) == 1){
+                System.out.println("Higher than: " + guess);
+                if(min == guess){
+                    min++;
+                } else{
+                    min = guess;
+                }
+            } else {
+                System.out.println("Smaller than: " + guess);
+                max = guess;
+            }
+            guess = (min + max)/2;
+        }
+        System.out.println("The number was: " + guess);
     }
 }
