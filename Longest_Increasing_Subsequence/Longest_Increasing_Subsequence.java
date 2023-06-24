176c1666ea1967962b56d2f0dd1b3d2e367a5b80
QuenWi <116632832+QuenWi@users.noreply.github.com>
2023-06-24 13:55:35 +0200

    longest inceasing subsequence
    start

---
+++ Longest_Increasing_Subsequence/Longest_Increasing_Subsequence.java
@@ -0,0 +1,55 @@
+package Longest_Increasing_Subsequence;
+
+public class Longest_Increasing_Subsequence {
+
+    //return the longest increasing subsquence inside an unsorted array. The sequence has not to be continues.
+    //Example {1, 4, 2, 3} The longest subsequence would be {1, 2, 3}
+    public static <T extends Number> Integer longest_Increasing_Subsequence(T[] array){
+        Integer result = 1;
+        return result;
+    }
+
+
+    public static <T extends Number> boolean compare(T nr1, T nr2){
+        boolean result = false;
+        if(nr1 instanceof Float){
+            if(nr1.floatValue() < nr2.floatValue()){
+                return true;
+            } else{
+                return false;
+            }
+        } else if(nr1 instanceof Double){
+            if(nr1.doubleValue() < nr2.doubleValue()){
+                return true;
+            } else{
+                return false;
+            }
+        } else if(nr1 instanceof Integer){
+            if(nr1.intValue() < nr2.intValue()){
+                return true;
+            } else{
+                return false;
+            }
+        } else if(nr1 instanceof Short){
+            if(nr1.longValue() < nr2.longValue()){
+                return true;
+            } else{
+                return false;
+            }
+        } else if(nr1 instanceof Byte){
+            if(nr1.byteValue() < nr2.byteValue()){
+                return true;
+            } else{
+                return false;
+            }
+        } else if(nr1 instanceof Short){
+            if(nr1.shortValue() < nr2.shortValue()){
+                return true;
+            } else{
+                return false;
+            }
+        }
+        return result;
+    }
+
+}
---
+++ Longest_Increasing_Subsequence/Test_Longest_Increasing_Subsequence.java
@@ -0,0 +1,8 @@
+package Longest_Increasing_Subsequence;
+
+public class Test_Longest_Increasing_Subsequence {
+
+    public static void main(String[] args) {
+
+    }
+}
